6eb6338a0b43be2bca877f92d65e7b3cad531371
gitneh <kc-nehuen@hotmail.com>
2021-11-24 16:28:46 -0300

    desafio 6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ItemDetailContainer from "../components/ItemDetailContainer";
 import ItemListContainer from "../components/ItemListContainer";
 import NavBar from "../components/NavBar";
 
@@ -10,7 +9,6 @@ const Home = () => {
             <Routes>
                 <Route path='/' element={<ItemListContainer />} />
                 <Route path='/category/:idCategory' element={<ItemListContainer />} />
-                <Route path='/item/:idItem' element={<ItemDetailContainer />} />
             </Routes>
         </BrowserRouter>
     );
